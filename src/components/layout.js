2844d35ad3e9e0b90a67e940307f61f91c9e6b7d
JayCuthrell <jay@cuthrell.com>
2021-09-26 01:40:47 +0000

    adding sitelogo from layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import SiteLogo from "../components/sitelogo"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -8,9 +9,10 @@ const Layout = ({ location, title, children }) => {
 
   if (isRootPath) {
     header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
+      <SiteLogo />
+//      <h1 className="main-heading">
+//        <Link to="/">{title}</Link>
+//      </h1>
     )
   } else {
     header = (
