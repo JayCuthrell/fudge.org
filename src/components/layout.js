fc58eb0672ff1044ad900409f96eab2ef1ea5121
JayCuthrell <jay@cuthrell.com>
2021-09-26 00:25:25 +0000

    footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,12 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © 1998 - {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://www.gatsbyjs.com">Gatsby Cloud</a>
+	  <p>
+	  <a href="https://jaycuthrell.com/disclosure">#include &lt;disclaimer.h&gt;</a>
+	  </p>
       </footer>
     </div>
   )
