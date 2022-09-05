19fc4471bc44904e767692d268f43b7bf1723207
JayCuthrell <jay@cuthrell.com>
2022-09-05 23:15:49 +0000

    sitemap advanced

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,7 +34,7 @@ module.exports = {
         },
       },
     }, 
-    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-advanced-sitemap`,
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
